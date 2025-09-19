5a73f6c05f77d0f6ee5e75aad7a1011c4ec01c64
iLorenzz <ldutra0912@gmail.com>
2025-09-18 21:08:34 -0300

    feat: implement task service

---
+++ todosimple/src/main/java/com/iLorenzz/todosimple/services/TaskService.java
@@ -0,0 +1,52 @@
+package com.iLorenzz.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iLorenzz.todosimple.models.Task;
+import com.iLorenzz.todosimple.models.User;
+import com.iLorenzz.todosimple.repositories.TaskRepository;
+
+import jakarta.transaction.Transactional;
+@Service
+public class TaskService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException(
+            "No such task found!"
+        ));
+    }
+
+    @Transactional
+    public Task create(Task obj){
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj){
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't delete related entities!");
+        }
+    }
+}
